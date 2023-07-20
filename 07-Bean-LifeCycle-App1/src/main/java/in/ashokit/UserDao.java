2402205e46ba9c8e9e55974903bbdecdf2482722
Mustafa11233 <140052516+Mustafa11233@users.noreply.github.com>
2023-07-20 20:24:22 +0530

    Add files via upload

---
+++ 07-Bean-LifeCycle-App1/src/main/java/in/ashokit/UserDao.java
@@ -0,0 +1,22 @@
+package in.ashokit;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class UserDao  implements InitializingBean,DisposableBean{
+	
+	public void getData() {
+		System.out.println("getting data from db....");
+	}
+
+	public void destroy() throws Exception {
+		System.out.println("destroy method.....");
+		
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("init method.....");
+		
+	}
+
+}
